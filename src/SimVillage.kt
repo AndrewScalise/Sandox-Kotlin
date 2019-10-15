afef837e7d12f059354179a8198508d2ba61db07
Mugen <paradigmcomplex@gmail.com>
2019-10-15 16:35:11 -0500

    made higher order function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,10 +1,21 @@
 fun main(args: Array<String>) {
-    val greetingFunction = {
-        playerName: String, numBuildings: Int ->
-            val currentYear = 2019
-            println("Adding $numBuildings houses")
-            "Welcome to SimVillage, $playerName! (copyright $currentYear)"
-    }
 
-    println(greetingFunction("Guyal", 2))
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return {
+        playerName: String ->
+        val currentYear = 2019
+        numBuildings+=1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
 }
